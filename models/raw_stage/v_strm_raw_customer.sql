f480f6b16f9ae75f88aebd0c8b946f45041e35ea
pramalingam@phdata.io <pramalingam@phdata.io>
2023-04-15 02:10:11 +0000

    -try3

--- models/raw_stage/v_strm_raw_customer.sql
+++ models/raw_stage/v_strm_raw_customer.sql
@@ -1,5 +1,5 @@
 {{ config(
-    pre_hook="CREATE or replace STREAM strm_raw_customer ON view raw_customer SHOW_INITIAL_ROWS = TRUE "]
+    pre_hook="CREATE or replace STREAM strm_raw_customer ON view raw_customer SHOW_INITIAL_ROWS = TRUE"
 ) }}
 
 select * from {{ source('tpch_sample', 'strm_raw_customer') }}
