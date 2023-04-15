a6521175a88835a26d811630b3f661c8804c7a2f
pramalingam@phdata.io <pramalingam@phdata.io>
2023-04-15 07:08:41 +0000

    sgfss

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,13 @@
+
+
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
+    {%- if target.name == 'slim_ci_run' -%}
+
+        {{ target.name }}
+    
+    {%- elif custom_schema_name is none -%}
 
         {{ default_schema }}
 
@@ -12,3 +18,6 @@
     {%- endif -%}
 
 {%- endmacro %}
+
+
+
